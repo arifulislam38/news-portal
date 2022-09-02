09b6b1e8f84c1c6406702ca3526ac777ec7d8e46
arifulislam38 <mdarifulislam0238@gmail.com>
2022-09-03 03:55:20 +0600

    card data loaded

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// categoryList
 
 const loadCategory = async() =>{
     const url = `https://openapi.programming-hero.com/api/news/categories`;
@@ -11,8 +10,40 @@ const displayCategory = category =>{
         console.log(value.category_name);
         const categoryId = document.getElementById('categoryList');
         const li = document.createElement('li');
+        li.setAttribute('onClick',`showData('${value.category_id}')`);
         li.innerText = `${value.category_name}`;
         categoryId.appendChild(li);
     }
+}
+
+
+const showData = async(value) =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/${value}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const newsEs = data.data;
+    console.log(newsEs);
+    for(const news of newsEs){
+        console.log(news);
+        const displayNews = document.getElementById('news-display');
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card','mb-2');
+        cardDiv.innerHTML = `
+            <div class="row g-0">
+            <div class="col-md-3">
+                <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="This is an image">
+            </div>
+            <div class="col-md-7">
+            <div class="card-body">
+                <h5 class="card-title">${news.title}</h5>
+                <p class="card-text">${news.details}</p>
+                
+            </div>
+            </div>
+        </div>
+        `;
+        displayNews.appendChild(cardDiv);
+    }
+
 }
 loadCategory();
