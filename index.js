a09e3ef087d81168d83f2890c7f033de5f516fb4
arifulislam38 <mdarifulislam0238@gmail.com>
2022-09-03 12:08:27 +0600

    news sequence added

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const loadCategory = async() =>{
 }
 const displayCategory = category =>{
     for(const value of category){
-        console.log(value.category_name);
         const categoryId = document.getElementById('categoryList');
         const li = document.createElement('li');
         li.setAttribute('onClick',`showData('${value.category_id}','${value.category_name}')`);
@@ -27,6 +26,7 @@ const showData = async(value,catName) =>{
     const res = await fetch(url);
     const data = await res.json();
     const allNews = data.data;
+    allNews.sort((a, b) => b.total_view - a.total_view); // shorting the news by view
     const displayNews = document.getElementById('news-display');
     displayNews.innerHTML = '';
     for(const news of allNews){
@@ -40,7 +40,7 @@ const showData = async(value,catName) =>{
         cardDiv.innerHTML = `
         <div class="pointer row border rounded m-0 p-2">
             <div class="col-3 ps-0">
-                <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="This is an image">
+                <img src="${news.thumbnail_url? news.thumbnail_url:'image not found'}" class="img-fluid rounded-start" alt="This is an image">
             </div>
          <div class="col-9 pe-2 ps-0 d-grid align-items-center justify-content-center">
                 <div>
@@ -49,12 +49,12 @@ const showData = async(value,catName) =>{
                 </div>
                 <div class="d-flex align-items-center justify-content-between">
                     <p class="d-flex flex-column">
-                        <span>${news.author.name}</span>
-                        <span>${news.author.published_date}</span>
+                        <span>${news.author.name? news.author.name : "Author not found"}</span>
+                        <span>${news.author.published_date? news.author.published_date : "publishing date not found"}</span>
                     </p>
                     <p class="d-flex gap-1">
                         <span><i class="fa-regular fa-eye"></i></span>
-                        <span>${news.total_view}</span>
+                        <span>${news.total_view? news.total_view : 'No data found'}</span>
                     </p>
                     <p class="d-flex gap-1">
                         <i class="fa-solid fa-star"></i>
@@ -86,28 +86,26 @@ const modalData = async(newsId) =>{
     const newsData = data.data[0];
     const modalImage = document.getElementById('modal-image');
     modalImage.innerHTML = `
-        <img src="${newsData.image_url}"class="img-fluid">
+        <img src="${newsData.image_url? newsData.image_url: 'image not found'}"class="img-fluid">
     `;
     const modalText = document.getElementById('modal-text');
     const createP = document.createElement('p');
     createP.innerText = `${newsData.details}`;
     modalText.appendChild(createP);
 
-    const authorName = document.getElementById('authorName');
-    authorName.innerText = `${newsData.author.name}`;
 
     const publishDate = document.getElementById('publishDate');
-    publishDate.innerText = `${newsData.author.published_date}`;
+    publishDate.innerText = `${newsData.author.published_date? newsData.author.published_date : 'publishing date not found'}`;
 
     const badge = document.getElementById('badge');
-    badge.innerText = `${newsData.rating.badge}`;
+    badge.innerText = `${newsData.rating.badge? newsData.rating.badge: 'Badge not found'}`;
 
 
     const author = document.getElementById('author');
     author.innerHTML = `
-        <img src="${newsData.author.img}" style="width:40px; height:40px; border-radius:50%;">
-        <p class="mb-0">${newsData.author.name}</p>
-    `
+        <img src="${newsData.author.img? newsData.author.img: 'image not found'}" style="width:40px; height:40px; border-radius:50%;">
+        <p class="mb-0">${newsData.author.name? newsData.author.name: 'author not found'}</p>
+    `;
     
 }
 
